5e9045f0088574cb530afda627c7e675dfafe151
Débora Serra <debora.r.serra@gmail.com>
2021-12-16 19:28:26 -0300

    Add projects to portfolio

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ let links = document.querySelector('#my-projects');
 let projectLink = [
   "https://deboraserra.github.io/Project-lessons-learned/project-block3.html",
   "https://deboraserra.github.io/Project-pixel-art/index.html",
+  "https://deboraserra.github.io/Project-to-do-list/index.html",
+  "https://deboraserra.github.io/Project-mystery-letter/index.html",
+  "https://deboraserra.github.io/Project-meme-generator/index.html",
+  "https://deboraserra.github.io/Project-color-guess/index.html",
 ];
 let projectText = [
   'Project Lessons Learned',
   'Project Pixel Art',
+  'Project To Do List',
+  'Project Mystery Letter',
+  'Project Meme Generator',
+  'Project Color Guess',
 ]
 
 for (let i = 0; i < projectLink.length; i += 1) {
